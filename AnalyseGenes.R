e7ff5ac1d7ac5613fcf78d8ff19b92e7ca2ca76d
Charlotte Voinot <69500275+cvoinot@users.noreply.github.com>
2020-11-21 19:18:58 +0100

    Update AnalyseGenes.R
    Code en anglais

--- AnalyseGenes.R
+++ AnalyseGenes.R
@@ -1,9 +1,9 @@
-########### Etude de l'expression différentielle des gènes selon le groupe d'intérêt ##################
+########### Study of the differential expression of genes according to the group of interest ##################
 
 
-rm(list=ls()) ##Nettoyage de l'environnement
+rm(list=ls()) ## cleaning 
 
-## Chargement des librairies à utiliser 
+## Loading of the librairies 
 library("DESeq2")
 library(tidyverse)
 library(tibble)
@@ -15,30 +15,30 @@ setwd('C:/Users/maryo/OneDrive/3A/COURS/AMI2B/Reprohackaton/Projet')
 
 
 #### PART1 : IMPORTING DATA ####
-## Récupération de la table.counts
+## Loading of table.counts
 countData <- read.table("gene_output.counts", sep ="\t", skip=1, header=T, row.names = 1)
-countData <- countData[-(1:5)] ## éliminer les 5 premières colonnes inutiles
+countData <- countData[-(1:5)] ## Ã©liminer les 5 premiÃ¨res colonnes inutiles
 
-## Changer le nom des colonnes de CountData pour enlever le .bam
-colnames(countData) <- sub(".bam", "", colnames(countData)) ## on enlève les 4 derniers caractères i.e le ".bam"
+## Change the name of the columns of CountData to remove .bam
+colnames(countData) <- sub(".bam", "", colnames(countData)) ## on enlÃ¨ve les 4 derniers caractÃ¨res i.e le ".bam"
 #rownames(countData)[duplicated(rownames(countData))]
 
 
-## To get metadata and keep the columns of interest
+## Get metadata and keep the columns of interest
 metaData <- read.csv('SraRunTable.txt', header = TRUE, sep = "\t")
-metaData <- metaData[c("Run", "sf3b1_mutation_status")] #on ne garde que les colonnes d'intérêt
+metaData <- metaData[c("Run", "sf3b1_mutation_status")] #we only keep columns of interest
 
 
-## CONSTITUTION DES DEUX GROUPES : SF3B1 muté et WT
-## construire une colonne condition dans metadata pour pouvoir établir le design : chercher les échantillons qui sont WT
-condition <- sapply(metaData$sf3b1_mutation_status, function(x){ifelse(grepl(pattern = "WT",x),"SF3B1_WT","SF3B1_mutated")}) #si pas WT -> SF3B1 mutated
-# Transformer en factor pour pouvoir utiliser le design
+## THE CONSTITUTION OF TWO GROUPS : SF3B1 mutated et WT
+## Create a column for condition in metadata to establish the design : look for the mutated samples
+condition <- sapply(metaData$sf3b1_mutation_status, function(x){ifelse(grepl(pattern = "WT",x),"SF3B1_WT","SF3B1_mutated")}) #if not WT -> SF3B1 mutated
+# Turn into factor to use the design
 metaData$condition <- factor(condition, levels = c("SF3B1_WT","SF3B1_mutated")) 
 
 
-## Transformation de countData en matrice 
+## Transformation of countData into matrix
 countData_mat <- data.matrix(countData)
-## transformation des données en "DESeqDataSet objet"
+## Data transformation into "DESeqDataSet objet"
 dds <- DESeqDataSetFromMatrix(countData=countData_mat,
                               colData=metaData,
                               design=~ condition)
@@ -47,15 +47,15 @@ dds <- DESeqDataSetFromMatrix(countData=countData_mat,
 
 
 #### PART 2 : DIFFERENTIAL EXPRESSION ANALYSIS  ####
-dds <- DESeq(dds) ## faire l'analyse différentielle des gènes 
+dds <- DESeq(dds) ## to do the differential expression analysis of genes
 
-res <- results(dds, tidy =T) ## obtenir le logFC en imposant l'ordre, tidy = T pour avoir les id dans une colonne
+res <- results(dds, tidy =T) ## to getlogFC by imposing order, tidy = T to have the ID in the column
 
-## pour enlever les données NA dans colonne logFoldChange et pvalue
+## Remove the NA data of the logFoldChange and pvalue columns
 res <- res[-which(is.na(res$log2FoldChange)),]
 res <- res[-which(is.na(res$pvalue)),]
 
-## volcano plot avec les ENSEMBL id
+## Creation of a volcano plot with ENSEMBL id
 EnhancedVolcano(res,
                 lab = res[,1],
                 x = 'log2FoldChange',
@@ -65,7 +65,7 @@ EnhancedVolcano(res,
                 FCcutoff = log2(1.5),
                 labSize = 4.0)
 
-### to keep only topgenes from our DE analysis
+### Keep only topgenes from our DE analysis
 topgenes <- res[res[,"pvalue"] < 0.05, ]
 
 topgenes <- as.data.frame(topgenes)
@@ -77,19 +77,19 @@ topdowns <- topgenes %>%
   filter(log2FoldChange<(-(log2(1.5)))) %>% 
   arrange(log2FoldChange)
 
-## ajouter les deux types de gènes ensemble dans un même fichier de sortie
+## Gather the two types of genes in a same output file
 fichier_sortie <- rbind(topups,topdowns)
 
-## créer une colonne avec le nom des gènes
+## Create a column with the name of the genes
 hs <- org.Hs.eg.db
 ts <- select(hs, keys = fichier_sortie[,1], columns = c("ENSEMBL", "SYMBOL"), keytype = "ENSEMBL")
 ts <- ts[!duplicated(ts$ENSEMBL),]
 
-## ajouter cette colonne dans le fichier de sortie
+## Add this column in the output file
 fichier_sortie$Gene_symbol <- ts$SYMBOL
 colnames(fichier_sortie)[1] <- "ENSEMBL_id"
 
-### écrire le fichier de sortie dans votre working directory
+### Write the output file in our working directory
 write.table(fichier_sortie, file="./DE_analysis.txt", sep = "\t", row.names = FALSE)
 
 
@@ -98,32 +98,32 @@ write.table(fichier_sortie, file="./DE_analysis.txt", sep = "\t", row.names = FA
 
 
 ### PART3 : COMPARING WITH DE GENES FOUND IN THE ARTICLES ####
-## récupération données de l'article
+## Collect data from the article
 DE_article <- read.table('DE_articles.txt', header = TRUE, sep = "\t")
 
-## récupération des gènes en commun dans une liste 
+## Collect common genes in a list 
 gene_list <- paste(fichier_sortie[,1], collapse="|")
 common <- c(grep(gene_list, DE_article[,1], value =T))
 
 
-## récupérer les gènes en commun dans leur analyse et les ordonner alphabétiquement pour avoir le même ordre de gènes
+## Collect the analysis of common genes from the article and order it alphabetically
 data_article <- DE_article[which(DE_article$EnsEMBLID %in% common),]
 data_article <- data_article[order(data_article$EnsEMBLID),]
 
-## To add a new column with the log2FC values
+## Add a new column with the log2FC values
 data_article$Fold.Change <- as.numeric(data_article$Fold.Change)
 data_article$log2FoldChange <- sapply(data_article$Fold.Change, function(x) {-log2(x)})
 
 
-## récupérer les gènes en commun dans notre analyse perso et les ordonner alphabétiquement pour avoir le même ordre de gènes
+## Collect the common genes in our analysis and order it alphabetically to have the same order
 our_analysis <- fichier_sortie[which(fichier_sortie[,1] %in% common),]
 our_analysis <- our_analysis[order(our_analysis$row),]
 
-## fusion des deux dataframes
+## Fusion of the two dataframes
 data_merge <- cbind(data_article, our_analysis$log2FoldChange, our_analysis$pvalue)
 colnames(data_merge) <- c("ENSEMBL_id", "FAST.DB.STABLE_id", "Gene_Symbol", "Regulation", "Article.|FC|", 
                           "Article.pvalue", "Aliases&Synonyms", "Article.log2FC", "Our.log2FC", "Our.pvalue")
   
   
-### écrire le fichier de sortie dans votre working directory
+### Write the output file in our working directory
 write.table(data_merge, file="./DE_genes_comparison.txt", sep = "\t", row.names = FALSE)
